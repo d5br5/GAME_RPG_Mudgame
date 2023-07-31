350fbd2270648915448f4a0cc4d73b8d9e461377
Doh Kim <ehgud456456@naver.com>
2023-07-31 10:32:39 +0900

    [Refactor] add api directory

---
+++ api/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const fs = require("fs");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const port = process.env.PORT || 4000;
+const { constantManager } = require("../src/datas/Manager");
+const mongoURI = process.env.mongoURI;
+const { action } = require("../src/controller/action");
+const { signup } = require("../src/controller/sign");
+const { authentication } = require("../src/middle/auth");
+const { ranking } = require("../src/controller/ranking");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(express.static("./img/"));
+app.engine("html", require("ejs").renderFile);
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Successfully connected to mongodb")) // mongodb connection success
+  .catch((e) => console.error(e));
+
+app.get("/", async (req, res) => {
+  res.render("index", {
+    gameName: constantManager.gameName,
+    rankList: await ranking(),
+  });
+});
+
+app.get("/music", (req, res) => {
+  fs.readFile("./music/AcientForest.mp3", (error, data) => {
+    res.writeHead(200, { "Content-Type": "audio/mp3" });
+    res.end(data);
+  });
+});
+
+app.get("/game", (req, res) => {
+  res.render("game");
+});
+app.post("/signup", signup, (req, res) => {});
+app.post("/action", authentication, action);
+app.get("/ending", (req, res) => {
+  res.render("ending", { score: req.score });
+});
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
+});
+
+module.exports = app;
